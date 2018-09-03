2dd24c7149f4232f4a2a9828bbb2f52524a91740
Zeitzew Michael <ZeitzewMichaelA@JohnDeere.com>
2018-09-03 12:18:34 -0700

    add connect and disconnect events

--- server/server.js
+++ server/server.js
@@ -1,15 +1,26 @@
 const path = require('path');
 
 const publicPath = path.join(__dirname, '../public');
-
+const http = require('http'); // built in
 const express = require('express');
+const socketIO = require('socket.io');
 
 var app = express();
 const port = process.env.PORT || 3000;
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, ()  => {
+io.on('connection', (socket) => {
+    console.log('hi');
+
+    socket.on('disconnect', () => {
+        console.log('user sicoketnect');
+    });
+});
+
+server.listen(port, ()  => {
     console.log(`Started on port ${port}`);
 });
 
