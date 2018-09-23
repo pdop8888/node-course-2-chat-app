e823eada638e11634bebc38518ee2eea789bfedf
Zeitzew Michael <ZeitzewMichaelA@JohnDeere.com>
2018-09-23 14:25:02 -0700

    add form for msgs and show browser

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,30 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
     console.log('newMessage', message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from} ${message.text}`);
+
+    jQuery('#messages').append(li);
 });
+
+// socket.emit('createMessage', 
+//     {
+//       from: 'Frank',
+//       text: 'Hi'    
+//     },
+//     // generateMessage('User','screw your chat'),
+//     function (data) {
+//       console.log('Got it', data);
+//     }
+//   );
+
+  jQuery('#message-form').on('submit', function (e) {
+    e.preventDefault(); // no refresh
+
+    socket.emit('createMessage', {
+      from: 'User',
+      text: jQuery('[name=message]').val() // selector
+    }, function () {
+
+    });
+  });
