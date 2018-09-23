6573ab8c262a20350830b19403d0505d62686c3e
Zeitzew Michael <ZeitzewMichaelA@JohnDeere.com>
2018-09-23 13:43:37 -0700

    create generateMessage

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const http = require('http'); // built in
 const express = require('express');
 const socketIO = require('socket.io');
 
+const{generateMessage} = require('./utils/message');
+
 var app = express();
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -14,25 +16,13 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
 
-    socket.emit('newMessage', {
-        from: 'Admin',
-        text: 'Welcome to the chat',
-        createdAt: new Date().getTime()
-    });
+    socket.emit('newMessage', generateMessage('Admin','Welcome to the chat'));
 
-    socket.broadcast.emit('newMessage', {
-        from: 'Admin',
-        text: 'New user joined.',
-        createdAt: new Date().getTime()
-    });
+    socket.broadcast.emit('newMessage', generateMessage('Admin','New user joined'));
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
+        io.emit('newMessage', generateMessage(message.from,message.text));
     });
 
     socket.on('disconnect', () => {
