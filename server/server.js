c12cc2b19cbfd6081e62f7c1fbf34e09e7f8f246
Zeitzew Michael <ZeitzewMichaelA@JohnDeere.com>
2018-09-23 13:10:13 -0700

    changes to server for new user connect

--- server/server.js
+++ server/server.js
@@ -13,11 +13,22 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-    console.log('new user connected');
+
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined.',
+        createdAt: new Date().getTime()
+    });
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
-        socket.broadcast.emit('newMessage', {
+        io.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
